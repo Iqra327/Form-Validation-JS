a5ddf6ccf6bdb9cffcc4fa2fc3ac14db0e578990
Iqra327 <iqrarasheed219@gmail.com>
2024-07-31 22:49:07 +0500

    initial-commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// const inputName = document.getElementById('fullName');
+// const inputEmail =  document.getElementById('email');
+// const inputPassowrd = document.getElementById('password');
+// const createAccountBtn = document.querySelector('.js-create-account-btn');
+
+// let personName;
+// let personEmail;
+// let accountPswrd;
+
+// function allInputs(){
+//   inputName.addEventListener('input', () => {
+//     personName = inputName.value;
+//   });
+  
+//   inputEmail.addEventListener('input', () =>{
+//     personEmail = inputEmail.value;
+//   })
+  
+//   inputPassowrd.addEventListener('input', () => {
+//     accountPswrd = inputPassowrd.value;
+//   })
+//   return personName, personEmail, accountPswrd; 
+// };
+
+// createAccountBtn.addEventListener('click', () => {
+//   if(allInputs()){
+//     alert("Your account has been successfully created!");
+//   }else{
+//     alert("Fill all inputs");
+//   }
+// })
+

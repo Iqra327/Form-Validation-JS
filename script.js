cbd4a33d2c8f5f35f1134ecac7c466cc281171c2
Iqra327 <iqrarasheed219@gmail.com>
2024-08-01 11:48:38 +0500

    form-validation-js

--- script.js
+++ script.js
@@ -1,32 +1,93 @@
-// const inputName = document.getElementById('fullName');
-// const inputEmail =  document.getElementById('email');
-// const inputPassowrd = document.getElementById('password');
-// const createAccountBtn = document.querySelector('.js-create-account-btn');
+const inputName = document.getElementById('fullName');
+const inputEmail =  document.getElementById('email');
+const inputPassowrd = document.getElementById('password');
+const inputConfirmPassword =  document.getElementById('confirmPassword');
+const createAccountBtn = document.querySelector('.js-create-account-btn');
+const eyeIcon = document.querySelector('.js-eye-icon');
+const nameError = document.querySelector('.js-nameError');
+const emailError = document.querySelector('.js-emailError');
+const passwordError = document.querySelector('.js-passError');
+const confirmPasswordError = document.querySelector('.js-confirmPassError');
 
-// let personName;
-// let personEmail;
-// let accountPswrd;
 
-// function allInputs(){
-//   inputName.addEventListener('input', () => {
-//     personName = inputName.value;
-//   });
+createAccountBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+ 
+  if(validateName() && validateEmail() && validatePassword() && validatePasswordConfirmation()){
+    alert("Form Submitted Successfully");
+  }
+})
+
+function validateName(){
+  let name = inputName.value;
+
+  if(!name){
+    nameError.innerHTML = "Name is required!";
+    nameError.previousElementSibling.classList.add('fa-xmark');
+    return false;
+  }
+
+  if(!/\s/.test(name)){
+    nameError.innerHTML = "Write full name!";
+    nameError.previousElementSibling.classList.add('fa-xmark');
+    return false;
+  }else{
+    nameError.previousElementSibling.classList.add('fa-check');
+    nameError.innerHTML = '';
+  }
+  return true;
+}
+
+function validateEmail(){
+  let email = inputEmail.value;
+  if(!email){
+    emailError.innerHTML = 'Email is required';
+    emailError.previousElementSibling.classList.add('fa-xmark');
+    return false;
+  }
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+  if(!emailPattern.test(email)){
+    emailError.innerHTML = 'Enter valid email';
+    emailError.previousElementSibling.classList.add('fa-xmark');
+    return false;
+  }else{
+    emailError.previousElementSibling.classList.add('fa-check');
+    emailError.innerHTML = '';
+  }
+  return true;
+}
+
+let password;
+
+function validatePassword(){
+  password = inputPassowrd.value;
+  if(!password){
+    passwordError.innerHTML = 'Password is required';
+    passwordError.previousElementSibling.classList.add('fa-xmark');
+    return false;
+  }
   
-//   inputEmail.addEventListener('input', () =>{
-//     personEmail = inputEmail.value;
-//   })
-  
-//   inputPassowrd.addEventListener('input', () => {
-//     accountPswrd = inputPassowrd.value;
-//   })
-//   return personName, personEmail, accountPswrd; 
-// };
+  const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/;
+  if(!passwordPattern.test(password)){
+    passwordError.previousElementSibling.classList.add('fa-xmark');
+    passwordError.innerHTML = 'Password must contain capital letters, small letters, numbers and symbols';
+    return false;
+  }else{
+    passwordError.innerHTML = '';
+    passwordError.previousElementSibling.classList.add('fa-check');
+  }
+  return true;
+}
 
-// createAccountBtn.addEventListener('click', () => {
-//   if(allInputs()){
-//     alert("Your account has been successfully created!");
-//   }else{
-//     alert("Fill all inputs");
-//   }
-// })
-
+function validatePasswordConfirmation(){
+  let confirmPassword = inputConfirmPassword.value;
+  if(confirmPassword !== password){
+    confirmPasswordError.innerHTML = `Password doesn't match`;
+    confirmPasswordError.previousElementSibling.classList.add('fa-xmark');
+    return false;
+  }else{
+    confirmPasswordError.innerHTML = '';
+    confirmPasswordError.previousElementSibling.classList.add('fa-check');
+  }
+  return true;
+}
